1b94cc325ef889dbc7c0997dac613e56694d539a
mariamiamore <165545199+mariamiamore@users.noreply.github.com>
2024-03-31 15:15:17 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿namespace number3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Converter");
+            while (true)
+            {
+                Console.WriteLine("1. seconds to  minutes");
+                Console.WriteLine("2. minutes to hours");
+                Console.WriteLine("3. hours to days");
+                Console.WriteLine("4. days to months");
+                Console.Write("Please choose what to convert: ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("You chose option 1.");
+                        {
+                            Console.Write("\nNumber of seconds: ");
+                            double seconds = Convert.ToDouble(Console.ReadLine());
+
+                            double minutes = seconds / 60.0;
+                            Console.WriteLine(minutes + " minute/s");
+                        }
+                        break;
+
+                    case "2":
+                        Console.WriteLine("You chose option 2.");
+                        {
+                            Console.Write("\nNumber of minutes: ");
+                            double minutes = Convert.ToDouble(Console.ReadLine());
+
+                            double hours = minutes / 60.0;
+                            Console.WriteLine(hours + " hour/s");
+                        }
+                        break;
+
+                    case "3":
+                        Console.WriteLine("You chose option 3.");
+                        {
+                            Console.Write("\nNumber of hours: ");
+                            double hours = Convert.ToDouble(Console.ReadLine());
+
+                            double days = hours / 24.0;
+                            Console.WriteLine(days + " day/s");
+                        }
+                        break;
+
+                    case "4":
+                        Console.WriteLine("You chose option 4.");
+                        {
+                            Console.Write("\nNumber of days: ");
+                            double days = Convert.ToDouble(Console.ReadLine());
+
+                            double months = days / 30.0;
+                            Console.WriteLine(months + " month/s");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice. Please choose again.");
+                        continue;
+                }
+                break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# number3
+ a program that converts the ff: seconds to minutes; minutes to hours hours to days; days to months
